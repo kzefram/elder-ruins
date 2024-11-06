b1ab279dcd3e5cd7b5713d170d2b317687085a5b
kzefram <karenabourgeois@gmail.com>
2024-11-06 15:18:25 -0400

    creating repo to share the fun

---
+++ README.md
@@ -0,0 +1,64 @@
+# Elder Ruins RPG
+
+## Contents
+
+[Description](#description) | 
+[Links](#links) | 
+[Screenshots](#screenshots) | 
+[TechStack](#tech-stack) | 
+[Instructions](#instructions) | 
+[Authors](#authors) | 
+[Licence](#license) | 
+[Acknowledgements](#acknowledgements) |
+[Support](#support)
+
+## Description
+
+Welcome to Elder Ruins RPG! A game where you can go on a wild adventure into the ruins of The Elder City and fight fantastical beasts. Join us on this quest, humble adventurer!
+
+## Links
+
+## ScreenShots
+
+## TechStack
+
+![JavaScript](https://a11ybadges.com/badge?logo=javascript)
+![HTML5](https://a11ybadges.com/badge?logo=html5)
+![CSS3](https://a11ybadges.com/badge?logo=css3)
+![YouTube](https://a11ybadges.com/badge?logo=youtube)
+![Google Chrome](https://a11ybadges.com/badge?logo=googlechrome)
+
+## Instructions
+
+### To play with the code
+1. Fork the repo
+2. Copy the SSH link from the upper right of the code page
+3. On the command line, type 'git clone <paste ssh link> or <paste http link>'
+4. If you have it set up, type 'code .' on the command line to open up your text editor
+
+### play game
+1. read the text in the red box
+2. make your choices by pressing one of the black buttons
+3. repeat!
+
+## Credits
+
+Copyright 2024 to Karen and Zack Bourgeois
+- Karen Bourgeois (https://github.com/zefram)
+- Zack Bourgeois
+
+## License
+
+- [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](/licence.md)
+
+## Acknowledgements
+
+This is a base copy of a RPG game to learn beginner JavaScript. This is done with the following video - https://youtu.be/SYx885hX0OY?si=fbm2fTMCBPELoBrv - We have made changes to the story and some of the code and we would also like to thank [Beauvine - Beau Carnes](https://www.youtube.com/@BeauCarnes)
+
+## Support
+
+For support (serious inquires only), 
+
+- Email me: karenabourgeois@gmail.com  
+- Portfolio: http://www.karenbourgeois.rocks 
+- LinkedIn: https://www.LinkedIn.com/in/karen0bourgeois 
---
+++ script.js
@@ -0,0 +1,323 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monterHealth;
+let inventory = ["dagger"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+  {
+    name: "dagger",
+    power: 5,
+  },
+  {
+    name: "short-sword",
+    power: 30,
+  },
+  {
+    name: "long-bow",
+    power: 50,
+  },
+  {
+    name: "long-sword",
+    power: 100,
+  },
+];
+
+const monsters = [
+  {
+    name: "slime cube",
+    level: 2,
+    health: 15,
+  },
+  {
+    name: "beast hybrids",
+    level: 8,
+    health: 60,
+  },
+  {
+    name: "king of dragons",
+    level: 20,
+    health: 300,
+  },
+];
+
+const locations = [
+  {
+    name: "capital city",
+    "button text": [
+      "Go to pile of supplies",
+      "Go to a path",
+      "Fight king of dragons dragon",
+    ],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the capital city. You see a pile of supplies.",
+  },
+  {
+    name: "pile of supplies",
+    "button text": [
+      "Buy 10 health (10 gold coins)",
+      "Buy weapon (30 gold coins)",
+      "Go to capital city",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You walk up to the pile of supplies. You see a rat.",
+  },
+  {
+    name: "a path",
+    "button text": [
+      "Fight slime cube",
+      "Fight beast hybrid",
+      "Go to capital city",
+    ],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You walk down a path. You see some monsters.",
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Block", "Flee"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster.",
+  },
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to capital city",
+      "Go to capital city",
+      "Go to capital city",
+    ],
+    "button functions": [goTown, goTown, easterEgg],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. ☠️",
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! 🎉",
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to capital city?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+  },
+];
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
+}
+
+function goTown() {
+  update(locations[0]);
+}
+
+function goStore() {
+  update(locations[1]);
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold coins to buy health.";
+  }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold coins to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold coins";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon, silly!";
+  }
+}
+
+function fightSlime() {
+  fighting = 0;
+  goFight();
+}
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterNameText.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+
+  if (isMonsterHit()) {
+    health -= getMonsterAttackValue(monsters[fighting].level);
+  } else {
+    text.innerText += " You miss.";
+  }
+
+  monsterHealth -=
+    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        fighting === 2 ? winGame() : defeatMonster();
+    }
+
+    if (Math.random() <= 0.1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks.";
+        currentWeapon--;
+  }
+}
+
+function getMonsterAttackValue(level) {
+  let hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit;
+}
+
+function isMonsterHit() {
+  return Math.random() > 0.2 || health < 20;
+}
+
+function dodge() {
+  text.innerText =
+    "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  let numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+
+  if (numbers.indexOf(guess) !== -1) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
